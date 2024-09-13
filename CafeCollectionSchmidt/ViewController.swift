a76b021d47babc08ef33840a886e2fedeb10409a
Ava Schmidt <ava.schmidt25@d155.org>
2024-09-13 05:43:08 -0700

    test

--- CafeCollectionSchmidt/ViewController.swift
+++ CafeCollectionSchmidt/ViewController.swift
@@ -1,13 +1,52 @@
-//starting cafe mini app!
 import UIKit
 
 class ViewController: UIViewController {
+//Creating Outlets
+    @IBOutlet weak var addOutlet: UIButton!
+   
+    @IBOutlet weak var menuOutlet: UITextView!
+    
+@IBOutlet weak var errorOutlet: UILabel!
 
+@IBOutlet weak var cartOutlet: UITextView!
+    
+@IBOutlet weak var itemNameOutlet: UITextField!
+
+@IBOutlet weak var quantityOutlet: UITextField!
+    
+    //creating parallel arrays
+    var foodNames = ["Coffee","Tea","Bagel","Crossiant","Scone"]
+    var foodPrices = [2.50,2.75,3.50,3.00,3.25]
+    
+    //creating empty dictionary
+    var cart = [String : Int]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       //Attempted to put text in textview...
+        //Did not show up? Why?
+        menuOutlet.text = ""
+        var i = 0
+        while i<foodNames.count{
+            print("Looping")
+            menuOutlet.text += "\(foodNames[i]) : \(foodPrices[i])"
+            i = i + 1 }
+        
+       }
+    
+    //Creating add button
+    @IBAction func addAction(_ sender: UIButton) {
+        //Resigning keyboard after click
+        itemNameOutlet.resignFirstResponder()
+        quantityOutlet.resignFirstResponder()
+        //getting input from user and storing into variables
+        var name = itemNameOutlet.text!
+        var qty = Int(quantityOutlet.text!)
+       
         
     }
-
+    
 
 }
 
--- CafeCollectionSchmidt/loginViewController.swift
+++ CafeCollectionSchmidt/loginViewController.swift
@@ -1,10 +1,3 @@
-//
-//  loginViewController.swift
-//  CafeCollectionSchmidt
-//
-//  Created by AVA SCHMIDT on 9/12/24.
-//
-
 import UIKit
 
 class loginViewController: UIViewController {
@@ -12,18 +5,7 @@ class loginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
