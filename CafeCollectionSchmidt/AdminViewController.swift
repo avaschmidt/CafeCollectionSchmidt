961d558874c026c76236772bd1e78035c4a4aee2
Ava Schmidt <avamschmidt07@gmail.com>
2024-09-16 08:44:10 -0500

    Fixed errors, started password structure

---
+++ CafeCollectionSchmidt/AdminViewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class AdminViewController: UIViewController {
+    @IBOutlet weak var passwordOutlet: UITextField!
+    var pass = "AvaIsCool"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func loginAction(_ sender: Any) {
+        if passwordOutlet.text == pass{
+            print("Yes")
+        }
+        else{
+            print("No")
+        }
+    }
+    
+
+}
